cb489d89311ec42185ce0668893e8715e1c11c6f
lokch05 <lokch0599@gmail.com>
2022-03-29 15:20:44 +0800

    add nearby clinic search

--- rasa-3.0.9/project/actions/actions.py
+++ rasa-3.0.9/project/actions/actions.py
@@ -11,6 +11,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
+import urllib.parse
 import http.client
 import json
 
@@ -25,6 +26,8 @@ clinics = [
         "careLine": 25601599,
         "latitude": 22.27648965449197,
         "longitude": 114.22847540253501,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Wan Tsui General Out-patient Clinic",
@@ -34,6 +37,8 @@ clinics = [
         "careLine": 28971058,
         "latitude": 22.263120748233476,
         "longitude": 114.23933339679101,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "G/F, Violet Peel General Out-patient Clinic",
@@ -43,6 +48,8 @@ clinics = [
         "careLine": 23871776,
         "latitude": 22.275328913447083,
         "longitude": 114.17775699741416,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Kennedy Town Jockey Club General Out-patient Clinic",
@@ -52,6 +59,8 @@ clinics = [
         "careLine": 70837500,
         "latitude": 22.281869430059388,
         "longitude": 114.12397929361025,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Aberdeen Jockey Club General Out-patient Clinic",
@@ -61,6 +70,8 @@ clinics = [
         "careLine": 70836500,
         "latitude": 22.24975652151861,
         "longitude": 114.15638976120827,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Robert Black General Out-patient Clinic",
@@ -70,6 +81,8 @@ clinics = [
         "careLine": 35064277,
         "latitude": 22.332467954700263,
         "longitude": 114.19566312317806,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "East Kowloon General Out-patient Clinic",
@@ -79,6 +92,8 @@ clinics = [
         "careLine": 35062989,
         "latitude": 22.3396002279426,
         "longitude": 114.20779189741147,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Wang Tau Hom Jockey Club General Out-patient Clinic",
@@ -88,6 +103,8 @@ clinics = [
         "careLine": 52784279,
         "latitude": 22.339072672465548,
         "longitude": 114.18535592685244,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Yau Ma Tei Jockey Club General Out-patient Clinic",
@@ -97,6 +114,8 @@ clinics = [
         "careLine": 35064989,
         "latitude": 22.309525452763694,
         "longitude": 114.16928844000961,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Kowloon Bay Health Centre General Out-patient Clinic",
@@ -106,6 +125,8 @@ clinics = [
         "careLine": 52152312,
         "latitude": 22.32863672853433,
         "longitude": 114.20689793911059,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Lam Tin Polyclinic General Out-patient Clinic",
@@ -115,6 +136,8 @@ clinics = [
         "careLine": 23701279,
         "latitude": 22.310518944543762,
         "longitude": 114.23334706428211,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Cheung Sha Wan Jockey Club General Out-patient Clinic",
@@ -124,6 +147,8 @@ clinics = [
         "careLine": 23878211,
         "latitude": 22.34067202091219,
         "longitude": 114.15858228820822,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Shek Kip Mei General Out-patient Clinic",
@@ -133,6 +158,8 @@ clinics = [
         "careLine": 27883023,
         "latitude": 22.330795581164676,
         "longitude": 114.16720348755547,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Tseung Kwan O (Po Ning Road) General Out-patient Clinic",
@@ -142,6 +169,8 @@ clinics = [
         "careLine": 95282106,
         "latitude": 22.31778434621669,
         "longitude": 114.26794570231405,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "South Kwai Chung Jockey Club General Out-patient Clinic",
@@ -151,6 +180,8 @@ clinics = [
         "careLine": 26157333,
         "latitude": 22.360854714524216,
         "longitude": 114.12532212685285,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Mrs Wu York Yu General Out-patient Clinic",
@@ -160,6 +191,8 @@ clinics = [
         "careLine": 24010124,
         "latitude": 22.376057611544752,
         "longitude": 114.1367170891989,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Tsing Yi Town General Out-patient Clinic",
@@ -169,6 +202,8 @@ clinics = [
         "careLine": 24346205,
         "latitude": 22.35441236586844,
         "longitude": 114.10592568022616,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Yuen Chau Kok General Out-patient Clinic",
@@ -178,6 +213,8 @@ clinics = [
         "careLine": 91472400,
         "latitude": 22.382326725110115,
         "longitude": 114.20163229434353,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Tai Po Jockey Club General Out-patient Clinic",
@@ -187,6 +224,8 @@ clinics = [
         "careLine": 66252483,
         "latitude": 22.453986048594217,
         "longitude": 114.16582318822114,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "1/F, Fanling Family Medicine Centre",
@@ -196,6 +235,8 @@ clinics = [
         "careLine": 60164814,
         "latitude": 22.49615829995044,
         "longitude": 114.13883851397394,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Yan Oi General Out-patient Clinic",
@@ -205,6 +246,8 @@ clinics = [
         "careLine": 70724784,
         "latitude": 22.391208065106806,
         "longitude": 113.97919525814908,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "1/F, Tin Shui Wai (Tin Yip Road) Community Health Centre",
@@ -214,6 +257,8 @@ clinics = [
         "careLine": 70724795,
         "latitude": 22.465441385172607,
         "longitude": 114.00578077225663,
+        "distance": {},
+        "duration": {},
     },
     {
         "name": "Madam Yung Fung Shee Health Centre",
@@ -223,9 +268,13 @@ clinics = [
         "careLine": 64680113,
         "latitude": 22.44090336541119,
         "longitude": 114.02699446184727,
+        "distance": {},
+        "duration": {},
     }
 
 ]
+
+key = "AIzaSyB9BuzmAq-fomht165r93txlOw_6DVygrA"
 
 
 class ActionAskSituation(Action):
@@ -265,57 +314,59 @@ class ActionFindClinic(Action):
     def name(self) -> Text:
         return "action_find_clinic"
 
-    def getInfo(self, area):
-        filtered_clinics = []
+    def getInfo(self, address):
+        filtered_clinic = []
+        conn = http.client.HTTPSConnection("maps.googleapis.com")
+        payload = ''
+        headers = {}
+        origin = urllib.parse.quote_plus(address, safe='')
 
         for clinic in clinics:
-            if clinic["area"] == area:
-                filtered_clinics.append(clinic)
+            destination = urllib.parse.quote_plus(clinic["name"], safe='')
+            conn.request(
+                "GET", f"/maps/api/directions/json?key={key}&destination={destination}&origin={origin}&language=en&region=hk&mode=walking", payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            r = json.loads(data.decode("utf-8"))
+            #print(json.dumps(r, indent=4, sort_keys=True))
 
-        return filtered_clinics
+            if(r["routes"] != []):
+                clinic["distance"] = r["routes"][0]["legs"][0]["distance"]
+                clinic["duration"] = r["routes"][0]["legs"][0]["duration"]
+                if(filtered_clinic == []):
+                    filtered_clinic = clinic
+                else:
+                    if(filtered_clinic["distance"]["value"] > clinic["distance"]["value"]):
+                        filtered_clinic = clinic
+            else:
+                break
+
+        return filtered_clinic
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        area = tracker.get_slot("area")
-        filtered_clinics = self.getInfo(area)
+        address_string = tracker.latest_message.get('text')
+        filtered_clinic = self.getInfo(address_string)
 
-        dispatcher.utter_message(
-            text=f"For clinics in {area}, you can go to the below clinics:")
-
-        for clinic in filtered_clinics:
-            name = clinic["name"]
-            address = clinic["address"]
-            tel = clinic["tel"]
-            careLine = clinic["careLine"]
+        if(filtered_clinic != []):
+            duration = filtered_clinic["duration"]["text"]
             dispatcher.utter_message(
-                text=f"Clinic name: {name}\nAddress: {address}\nBook appointment tel number: {tel}\nCare booking line: {careLine}")
+                text=f"We have found a designated near {address_string}, which is just about {duration} of walking.")
+
+            dispatcher.utter_message(text="Your nearest clinic is:")
             dispatcher.utter_message(
                 text="============================================================")
 
-        return [SlotSet(key="area", value=None)]
+            name = filtered_clinic["name"]
+            address = filtered_clinic["address"]
+            tel = filtered_clinic["tel"]
+            careLine = filtered_clinic["careLine"]
+            dispatcher.utter_message(
+                text=f"Clinic name: {name}\nAddress: {address}\nBook appointment tel number: {tel}\nCare booking line: {careLine}")
+        else:
+            dispatcher.utter_message(
+                text="Sorry. We could not find any designated clinic near you. Please try again next time")
 
-
-# class ActionAskBuildings(Action):
-
-#     def name(self) -> Text:
-#         return "action_ask_buildings"
-
-#     def getInfo(self):
-#         conn = http.client.HTTPConnection("covid19.peter-ng.com")
-#         payload = ''
-#         headers = {}
-#         conn.request("GET", "/info.json", payload, headers)
-#         res = conn.getresponse()
-#         data = res.read()
-#         r = json.loads(data.decode("utf-8"))
-#         return r
-
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         result = self.getInfo()
-
-#         dispatcher.utter_message(text=f"{result}")
+        return [SlotSet(key="address_string", value=None)]
